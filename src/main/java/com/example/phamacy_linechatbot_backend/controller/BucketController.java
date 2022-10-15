70e590246b1a3f82b439b77962fb4dc45678c85b
MK <anokalittle@gmail.com>
2022-10-15 14:25:07 +0700

    ✨ Image uploader POST method

---
+++ src/main/java/com/example/phamacy_linechatbot_backend/controller/BucketController.java
@@ -0,0 +1,24 @@
+package com.example.phamacy_linechatbot_backend.controller;
+
+import com.example.phamacy_linechatbot_backend.util.CloudStorageHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+@Controller
+public class BucketController {
+    @Autowired
+    CloudStorageHelper cloudStorageHelper;
+    @PostMapping("/uploadFile")
+    public ResponseEntity<?> uploadFile(@RequestPart(value = "file")
+                                        MultipartFile file) throws IOException, ServletException {
+        return
+                ResponseEntity.ok(this.cloudStorageHelper.getImageUrl(file,"image-uploader-e2ddb.appspot.com"));
+    }
+}
